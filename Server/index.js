4a9d38ab748d2d175c1f3a525083a8864ce37e79
sandhya <your@email.com>
2026-03-19 12:26:21 +0545

    login

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const { UserRoutes } = require("./App/router/web/UserRoute");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.use(express.json());
+
+app.use("/user",UserRoutes)
+
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Database Connected!"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
