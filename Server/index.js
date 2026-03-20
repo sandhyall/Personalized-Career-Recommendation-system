b1a47d8ccbef2f4b698168bd9277a080bd75f4e1
ankitaachhetri <ankita2980@gmail.com>
2026-03-20 10:28:53 +0545

    Added form validation successfully

--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,41 @@
+
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const { UserRoutes } = require("./App/router/web/UserRoute");
-require("dotenv").config();
 
 const app = express();
+
 
 app.use(
   cors({
     origin: "http://localhost:5173",
-  }),
+    credentials: true,
+  })
 );
 
 app.use(express.json());
 
-app.use("/user",UserRoutes)
+
+app.use("/user", UserRoutes);
+
+
+console.log("MONGO_URI =", process.env.MONGO_URI);
 
 
 mongoose
-  .connect(process.env.DB)
-  .then(() => console.log("Database Connected!"))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI.trim(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected! "))
+  .catch((err) => console.log("Mongo Error :", err));
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} `);
+});
